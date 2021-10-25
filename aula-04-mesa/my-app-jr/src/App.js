c72f33711a44923075554c204c1d1d598e1bfc7b
juniorvilas <log_junior@hotmail.com>
2021-10-25 12:16:35 -0300

    aula04Mesa

---
+++ aula-04-mesa/my-app-jr/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Img from './img/homem-feliz-dinheiro.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Img} alt="imagem" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
